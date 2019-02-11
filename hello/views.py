6ab84e63b781cf7a4b66998042dbd4df140d769f
Rogerio <rrgmax@bol.com.br>
2019-02-11 02:20:03 -0200

    Add functions for the /about and /contact routes  into heelo/views.py file

--- hello/views.py
+++ hello/views.py
@@ -2,8 +2,18 @@ from django.shortcuts import render
 from datetime import datetime
 from django.http import HttpResponse
 
+#def home(request):
+#    return HttpResponse("Hello, Django!")
+# Replace the existing home function with the one below
 def home(request):
-    return HttpResponse("Hello, Django!")
+    return render(request, "hello/home.html")
+
+def about(request):
+    return render(request, "hello/about.html")
+
+def contact(request):
+    return render(request, "hello/contact.html")
+
 
 # You can also remove the import re statement that's no longer used
 
