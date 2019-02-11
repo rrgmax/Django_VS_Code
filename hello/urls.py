ffc1822b8d9ce1ef1690500c98a6a20067e8df84
Rogerio <rrgmax@bol.com.br>
2019-02-11 04:32:00 -0200

    Replaced  the home_list_view variable to fiz an error

--- hello/urls.py
+++ hello/urls.py
@@ -2,6 +2,12 @@ from django.urls import path
 from hello import views
 from hello.models import LogMessage
 
+home_list_view = views.HomeListView.as_view(
+    queryset=LogMessage.objects.order_by("-log_date")[:5],  # :5 limits the results to the five most recent
+    context_object_name="message_list",
+    template_name="hello/home.html",
+)
+
 urlpatterns = [
     #path("", views.home, name="home"),
         # Replace the existing path for ""
@@ -11,9 +17,3 @@ urlpatterns = [
     path("contact/", views.contact, name="contact"),
     path("log/", views.log_message, name="log"),
 ]
-
-home_list_view = views.HomeListView.as_view(
-    queryset=LogMessage.objects.order_by("-log_date")[:5],  # :5 limits the results to the five most recent
-    context_object_name="message_list",
-    template_name="hello/home.html",
-)
