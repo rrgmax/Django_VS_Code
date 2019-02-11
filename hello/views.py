f3e9e96a7b1728687b14b347dcde4521acefae0b
Rogerio <rrgmax@bol.com.br>
2019-02-11 03:57:18 -0200

    Define the view named log_message into hello/views.py file

--- hello/views.py
+++ hello/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from datetime import datetime
 from django.http import HttpResponse
+# Add these to existing imports at the top of the file:
+from django.shortcuts import redirect
+from hello.forms import LogMessageForm
+from hello.models import LogMessage
 
 #def home(request):
 #    return HttpResponse("Hello, Django!")
@@ -26,3 +30,17 @@ def hello_there(request, name):
             'date': datetime.now()
         }
     )
+
+# Add this code elsewhere in the file:
+def log_message(request):
+    form = LogMessageForm(request.POST or None)
+
+    if request.method == "POST":
+        if form.is_valid():
+            message = form.save(commit=False)
+            message.log_date = datetime.now()
+            message.save()
+            return redirect("home")
+    else:
+        return render(request, "hello/log_message.html", {"form": form})
+    
