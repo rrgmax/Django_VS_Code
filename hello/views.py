d2717d5d4d87f9dd57283978bbb0f46f7303967a
Rogerio <rrgmax@bol.com.br>
2019-02-11 00:40:01 -0200

    Change the hello_there function to render the template

--- hello/views.py
+++ hello/views.py
@@ -1,23 +1,25 @@
 from django.shortcuts import render
-import re
 from datetime import datetime
 from django.http import HttpResponse
 
 def home(request):
     return HttpResponse("Hello, Django!")
 
+# You can also remove the import re statement that's no longer used
+
 def hello_there(request, name):
     now = datetime.now()
     formatted_now = now.strftime("%A, %d %B, %Y at %X")
 
-    # Filter the name argument to letters only using regular expressions. URL arguments
-    # can contain arbitrary text, so we restrict to safe characters only.
-    match_object = re.match("[a-zA-Z]+", name)
+    # BAD CODE! Avoid inline HTML for security reason, plus templates automatically escape HTML content.
+    content = "<strong>Hello there, " + name + "!</strong> It's " + formatted_now
 
-    if match_object:
-        clean_name = match_object.group(0)
-    else:
-        clean_name = "Friend"
-
-    content = "Hello there, " + clean_name + "! It's " + formatted_now
-    return HttpResponse(content)
+    return render(
+        request,
+        'hello/hello_there.html',
+        {
+            'title': 'Hello, Django',
+            'content': content,
+        }
+    )
+    
