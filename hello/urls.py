22c44ca3f312ecc9047e66385c4b843443f8ef57
Rogerio <rrgmax@bol.com.br>
2019-02-11 04:24:02 -0200

    Also in urls.py, make a variable for the new view

--- hello/urls.py
+++ hello/urls.py
@@ -9,3 +9,9 @@ urlpatterns = [
     path("contact/", views.contact, name="contact"),
     path("log/", views.log_message, name="log"),
 ]
+
+home_list_view = views.HomeListView.as_view(
+    queryset=LogMessage.objects.order_by("-log_date")[:5],  # :5 limits the results to the five most recent
+    context_object_name="message_list",
+    template_name="hello/home.html",
+)
