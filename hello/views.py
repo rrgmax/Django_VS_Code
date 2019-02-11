8de9dadc6bb2685b527fce3c5b0e319b3e2766ff
Rogerio <rrgmax@bol.com.br>
2019-02-11 00:56:59 -0200

    Change he hello_there.html file and reset the views.py

--- hello/views.py
+++ hello/views.py
@@ -8,18 +8,11 @@ def home(request):
 # You can also remove the import re statement that's no longer used
 
 def hello_there(request, name):
-    now = datetime.now()
-    formatted_now = now.strftime("%A, %d %B, %Y at %X")
-
-    # BAD CODE! Avoid inline HTML for security reason, plus templates automatically escape HTML content.
-    content = "<strong>Hello there, " + name + "!</strong> It's " + formatted_now
-
     return render(
         request,
         'hello/hello_there.html',
         {
-            'title': 'Hello, Django',
-            'content': content,
+            'name': name,
+            'date': datetime.now()
         }
     )
-    
